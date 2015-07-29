63b5983829442a9d1c60f31ab89fd68a30239464
Kim Ravn Hansen <kim@krhit.com>
2015-07-29 11:57:50 +0200

    Optimizations

--- Manager.php
+++ Manager.php
@@ -27,15 +27,11 @@ class Manager
 
     public function encrypt($plaintext, $key, $iv = null)
     {
-        $crypto = $this->getCrypto($key, $iv);
-        $iv = $crypto->getIv();
         return $this->getCrypto($key, $iv)->encrypt($plaintext);
     }
 
     public function decrypt($ciphertext, $key, $iv = null)
     {
-        $crypto = $this->getCrypto($key, $iv);
-        $iv = $crypto->getIv();
         return $this->getCrypto($key, $iv)->decrypt($ciphertext);
     }
 }
